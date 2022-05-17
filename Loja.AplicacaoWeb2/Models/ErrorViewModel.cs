853e4134275098163ad7f2c0642c5606d5c54d2f
FelipeRodriguesDeveloper <feliperodriguesdeveloper@hotmail.com>
2022-05-17 11:20:56 -0300

    Deletar projeto repetido

--- Loja.AplicacaoWeb2/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Loja.AplicacaoWeb2.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
